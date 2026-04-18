eae44353e5d80e071794b214e23daa9a05f1dc88
Dibyam Raj Ghimire <dibyamm99@gmail.com>
2026-04-18 16:39:30 +0545

    Remove test scripts with leaked credentials

--- test_pg_nabil_asc.py
+++
@@ -1,5 +0,0 @@
-import psycopg2
-conn = psycopg2.connect('postgresql://neondb_owner:npg_Gmn2i1rwNsbe@ep-rough-fire-a18ou8fq-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require')
-cur = conn.cursor()
-cur.execute("SELECT symbol, date, open, high, low, close, volume FROM stocks_stockdata WHERE category='stock' AND symbol='NABIL' ORDER BY date LIMIT 10;")
-print(cur.fetchall())
--- test_pg_schema.py
+++
@@ -1,5 +0,0 @@
-import psycopg2
-conn = psycopg2.connect('postgresql://neondb_owner:npg_Gmn2i1rwNsbe@ep-rough-fire-a18ou8fq-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require')
-cur = conn.cursor()
-cur.execute("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'stocks_stockdata';")
-print(cur.fetchall())
