9f64e5397d6ba3686e9804c50840c504d82b2702
Dibyam Raj Ghimire <dibyamm99@gmail.com>
2026-04-18 16:39:30 +0545

    Remove test scripts with leaked credentials

--- test_pg_index.py
+++
@@ -1,5 +0,0 @@
-import psycopg2
-conn = psycopg2.connect('postgresql://neondb_owner:npg_Gmn2i1rwNsbe@ep-rough-fire-a18ou8fq-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require')
-cur = conn.cursor()
-cur.execute("SELECT DISTINCT symbol FROM stocks_stockdata WHERE category='index';")
-print(cur.fetchall())
